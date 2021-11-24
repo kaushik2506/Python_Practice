5d7a825449c7a81bf50d7cd995b70db8e0544e0f
kaushik2506 <kaushikthakur65@gmail.com>
2021-11-25 02:47:29 +0530

    adding 2nd program

---
+++ 1_Input_Processing_and_Output/2_Counting_the_Number_of_Characters.py
@@ -0,0 +1,24 @@
+# take user input and return its value only if the user has entered atleast 1 character
+def take_input():
+    while(True):
+        print(" ")
+        input_string = input('What is the input string: ')
+        if len(input_string) == 0:
+            print(" ")
+            print("Please enter something")
+            continue
+        else:
+            return input_string
+        break
+
+
+# displays output that shows the input string and the number of characters the string contains.
+def count_chars(input_string):
+    print(" ")
+    print(f"{input_string} has {len(input_string)} characters.")
+    # print(f"{input_string} has {len(list(input_string))} characters.")  using list --> it break downs the string to individual chars
+
+
+print(" ")
+# call the count_user function that will first call the take_input function
+count_chars(take_input())
