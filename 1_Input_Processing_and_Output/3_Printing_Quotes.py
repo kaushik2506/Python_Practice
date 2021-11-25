a79192b42c9ab44ed2171e37f0f658bc8a839b0b
kaushik2506 <kaushikthakur65@gmail.com>
2021-11-25 22:52:58 +0530

    used helper dunction

--- 1_Input_Processing_and_Output/3_Printing_Quotes.py
+++ 1_Input_Processing_and_Output/3_Printing_Quotes.py
@@ -1,14 +1,14 @@
 def take_input():
-    #dictionary to be used for input quote and person
+    # dictionary to be used for input quote and person
     input_dict = {
         "quote": "What is the quote: ",
         "person": "Who said it: "
-        }
+    }
 
-    #this while loop is only for input_quote
+    # this while loop is only for input_quote
     while(True):
         print(" ")
-        #this variable calls the dictionary method to get the value of quote
+        # this variable calls the dictionary method to get the value of quote
         input_quote = input(input_dict.get("quote"))
         if len(input_quote) == 0:
             print(" ")
@@ -16,9 +16,9 @@ def take_input():
             print(" ")
             continue
         else:
-            #this while loop is only for input_person
+            # this while loop is only for input_person
             while(True):
-                #this variable calls the dictionary method to get the value of person who said that quote
+                # this variable calls the dictionary method to get the value of person who said that quote
                 input_person = input(input_dict.get("person"))
                 if len(input_person) == 0:
                     print(" ")
@@ -28,7 +28,7 @@ def take_input():
                 break
         break
     # here, we return a list with two values
-    return [input_quote, input_person];
+    return [input_quote, input_person]
 
 
 def print_quotes(input_quote, input_person):
@@ -40,5 +40,3 @@ def print_quotes(input_quote, input_person):
 returned_value = take_input()
 # this function takes two values that is returned from the list
 print_quotes(returned_value[0], returned_value[1])
-
-
